84c6a54fe3aa3f47230670bf3f2b3309ff09fb93
Argent Lo <34392383+ArgentLo@users.noreply.github.com>
2021-04-10 09:11:27 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-sudo python3 train.py --gpu=0 --dataset=data/pinterest.npz --pretrain=pretrain/pinterest_e50.npz --neg=4 --hops=2 --iters=1 --batch_size=128 --use_unpop_weight=True --logdir=result/CMN_citeulike
+python3 train.py --gpu=0 --dataset=data/pinterest.npz --pretrain=pretrain/pinterest_e50.npz --neg=4 --hops=2 --iters=1 --batch_size=128 --use_unpop_weight=True --logdir=result/CMN_citeulike
 
 # Options for diff datasets:
 #--dataset=data/citeulike-a.npz --pretrain=pretrain/citeulike-a_e50.npz --neg=6
